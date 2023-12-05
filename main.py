41670448b97ed0f9fb50a94b3e41e180fd6b69ae
khill765 <131305116+khill765@users.noreply.github.com>
2023-12-04 23:22:22 -0500

    - Added base window for app - Added test button (no functionality) - Made the window resizable - Added a bitmap icon - Added a title - Changed default size to 600x500

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("Hello World")
-print("Hello World")
+import tkinter as tk
+
+
+class WeatherApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('600x500')
+        self.title("Ordinary Weather")
+        self.iconbitmap("WeatherAppIcon.ico")
+        self.resizable(True, True)
+        button = tk.Button(self, text= "test")
+        button.pack()
+
+
+def main():
+    WeatherApp().mainloop()
+
+
+if __name__ == '__main__':
+    main()
